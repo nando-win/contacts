3b31dda5f2001fa9910f416c6af759daa5764c9e
Fernando Bezerra <fernando.bezerra@atlantatecnologia.com.br>
2020-12-21 16:49:49 -0300

    Creating DB

--- lib/helpers/contact_helper.dart
+++ lib/helpers/contact_helper.dart
@@ -1,5 +1,7 @@
+import 'package:path/path.dart';
 import 'package:sqflite/sqflite.dart';
 
+final String contactTable = "contactTable";
 final String idColumn = "idColumn";
 final String nameColumn = "nameColumn";
 final String emailColumn = "emailColumn";
@@ -7,18 +9,44 @@ final String phoneColumn = "phoneColumn";
 final String imgColumn = "imgColumn";
 
 class ContactHelper {
+  static final ContactHelper _instance = ContactHelper.internal();
 
+  factory ContactHelper() => _instance;
+
+  ContactHelper.internal();
+
+  Database _db;
+
+  Future<Database> get db async {
+    if (_db != null) {
+      return _db;
+    } else {
+      _db = await initDb();
+      return _db;
+    }
+  }
+
+  Future<Database> initDb() async {
+    final databasesPath = await getDatabasesPath();
+    final path = join(databasesPath, "contacts.db");
+
+    return await openDatabase(path, version: 1,
+        onCreate: (Database db, int newerVersion) async {
+      await db.execute(
+          "CREATE TABLE $contactTable($idColumn INTEGER PRIMARY KEY, $nameColumn TEXT, $emailColumn Text"
+          "$phoneColumn TEXT, $imgColumn TEXT)");
+    });
+  }
 }
 
 class Contact {
-
   int id;
   String name;
   String email;
   String phone;
   String img;
 
-  Contact.fromMap(Map map){
+  Contact.fromMap(Map map) {
     id = map[idColumn];
     name = map[nameColumn];
     email = map[emailColumn];
@@ -33,7 +61,7 @@ class Contact {
       phoneColumn: phone,
       imgColumn: img
     };
-    if(id != null) {
+    if (id != null) {
       map[idColumn] = id;
     }
     return map;
